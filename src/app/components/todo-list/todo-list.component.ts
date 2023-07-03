7f81fa2310c7a0b4520b03f098bc3ac48d7216ce
Asaf Abekasis <72415412+Asafabekasis@users.noreply.github.com>
2023-07-03 22:21:16 +0300

    Update todo-list.component.ts

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -12,7 +12,8 @@ import { Todo, TodoService } from 'src/app/services/todo.service'
 export class TodoListComponent implements OnInit {
   todos$: Observable<Todo[]> | undefined
  
-  constructor(private todoService: TodoService) {this.todos$ = this.todoService.todos$;}
+  constructor(private todoService: TodoService)
+   {this.todos$ = this.todoService.todos$;}
  
   ngOnInit() {
     
